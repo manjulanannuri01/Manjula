2e2d0d8cf0c87da152610cf4da87460ba00ed9cc
yanadimanjula <153710196+yanadimanjula@users.noreply.github.com>
2023-12-13 15:19:18 +0530

    Add files via upload

---
+++ SampleExample-1/src/main/java/com/example/demo/SampleExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleExample1Application.class, args);
+	}
+
+}
